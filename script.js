1d5d40489f05e30ea2be5eddeae1a716d36209c3
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2025-04-01 19:06:40 -0700

    ([KG]) Added a section fo text, and image, and a link element to the HTMl file and then linked them in my javascript file.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let titleElement = document.getElementById("title");
 console.log(titleElement)
+
+let bodyElement = document.getElementById("bodytext");
+console.log(bodyElement)
+
+let imageElement = document.getElementById("img");
+console.log(imageElement)
+
+let linkElement = document.getElementById("url");
+console.log(linkElement)
